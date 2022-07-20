6eafbe3dc07e35df1dd38e65b6e37870a588242d
группы стационара <st@itstep.lan>
2022-07-20 19:52:38 +0300

    Changed name buttons

--- src/components/ButtonProps/ButtonProps.tsx
+++ src/components/ButtonProps/ButtonProps.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ButtonProps.css';
 
 const ButtonProps = () => {
-    return <button className="my-button2">Click</button>;
+    return <button className="my-button2">Click2</button>;
 };
 
 export default ButtonProps;
