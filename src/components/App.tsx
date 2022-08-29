b3b6287fd458d00b6354d07fe51009cfbe526387
iaveryanova <iolantazhiznevskaya@gmail.com>
2022-08-29 21:46:51 +0300

    Added Modal, Context, Login

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import Context from '../context/context';
 import './App.css';
 import AppRoutes from './AppRoutes';
 import NavBar from './NavBar';
+import { useState } from 'react';
+import Modal from './Modal';
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
+    <Context.Provider value={{openModal, setOpenModal}}>
     <div className='App container'>
       <NavBar />
-      <h1 className='mt-5'>My React App</h1>
       <AppRoutes />
+      <Modal />
     </div>
+    </Context.Provider>
   );
 }
 
