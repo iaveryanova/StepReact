8b4a7fa4e20734f91ad55987f3d6e2eb61992d73
iaveryanova <iolantazhiznevskaya@gmail.com>
2022-08-31 21:46:43 +0300

    Add LogIn/LogOut

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Context from '../context/context';
 import './App.css';
 import AppRoutes from './AppRoutes';
@@ -8,8 +8,15 @@ import Modal from './Modal';
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
+  const [isLoginUser, setIsLoginUser] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoginUser(true);
+    }
+  }, [])
   return (
-    <Context.Provider value={{openModal, setOpenModal}}>
+    <Context.Provider value={{openModal, setOpenModal, isLoginUser, setIsLoginUser}}>
     <div className='App container'>
       <NavBar />
       <AppRoutes />
